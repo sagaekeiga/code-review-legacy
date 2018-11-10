f540adf21e84809a24e1d96359ff058d1699c8e1
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-11-10 19:57:48 +0900

    fix: remove debug

--- app/models/reviewees/github_account.rb
+++ app/models/reviewees/github_account.rb
@@ -41,7 +41,6 @@ class Reviewees::GithubAccount < ApplicationRecord
   # ClassMethods
   # -------------------------------------------------------------------------------
   def self.find_for_oauth(auth)
-    Rails.logger.debug auth
     github_account = find_or_initialize_by(owner_id: auth['extra']['raw_info']['id'])
     github_account.assign_attributes(
       login: auth['extra']['raw_info']['login'],
