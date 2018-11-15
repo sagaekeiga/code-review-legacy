e0928509dd8e051dc619d376612638add66bdbb7
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-11-15 14:33:44 +0900

    fix: add replies.scss

--- config/routes.rb
+++ config/routes.rb
@@ -83,10 +83,10 @@ Rails.application.routes.draw do
       resources :pulls, only: %i(show update), param: :token do
         get :files
         resources :reviews, only: %i(create show edit update) do
+          get :replies
           collection do
             get :view_check
             get :file, to: 'reviews#new'
-            get :replies
           end
         end
         resources :comments, only: %i(create update destroy)
