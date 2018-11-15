bff487926af1d27a390ac269222e3cc91928e57b
sagae minami <gw.feltmyheart@gmail.com>
2018-11-15 21:30:43 +0900

    fix:rename_check_active

--- app/controllers/reviewers/base_controller.rb
+++ app/controllers/reviewers/base_controller.rb
@@ -1,13 +1,13 @@
 class Reviewers::BaseController < ApplicationController
   before_action :authenticate_reviewer!
   before_action :connect_github!
-  before_action :check_active
+  before_action :check_reviewer_status
 
   def connect_github!
     redirect_to reviewers_integrations_url, danger: t('reviewers.integrations.alert.danger') if current_reviewer.github_account.nil?
   end
 
-  def check_active
+  def check_reviewer_status
     redirect_to :reviewers_pending, notice: t('reviewers.pending.alert.danger') if current_reviewer.pending?
   end
 end
--- app/controllers/reviewers_controller.rb
+++ app/controllers/reviewers_controller.rb
@@ -1,6 +1,6 @@
 class ReviewersController < Reviewers::BaseController
   skip_before_action :connect_github!, only: %i(integrations)
-  skip_before_action :check_active, only: %i(pending integrations)
+  skip_before_action :check_reviewer_status, only: %i(pending integrations)
 
   def dashboard
     @pulls = Pull.order(created_at: :desc).page(params[:page])
