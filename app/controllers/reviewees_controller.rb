31f0f440fa7960b9976321e2799590f12bc7939d
sagae minami <gw.feltmyheart@gmail.com>
2018-11-18 01:17:42 +0900

    fix:reviewee_controller

--- app/controllers/reviewees_controller.rb
+++ app/controllers/reviewees_controller.rb
@@ -2,7 +2,7 @@ class RevieweesController < Reviewees::BaseController
   skip_before_action :connect_github!, only: %i(integrations)
 
   def dashboard
-    @reviewee = current_reviewee.decorate
+    @repos = current_reviewee.repos
   end
 
   def integrations
