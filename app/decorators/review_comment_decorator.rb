a776c8919e544a125198d89a1bf933a9df89a10c
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-11-20 22:07:06 +0900

    fix: modified review_decorator

--- app/decorators/review_comment_decorator.rb
+++ app/decorators/review_comment_decorator.rb
@@ -16,4 +16,29 @@ class ReviewCommentDecorator < ApplicationDecorator
   def step_image
     has_unread_replies? ? 'warning.png' : 'checked.png'
   end
+
+  def set_read_message
+    if has_unread_replies?
+      content_tag(:span, (I18n.t 'reviewers.reviews.replies.unread_messages', message_count: count_unread_replies), class: 'unread')
+    else
+      content_tag(:span, (I18n.t 'reviewers.reviews.replies.read_messages'), class: 'read')
+    end
+  end
+
+  def set_active
+    has_unread_replies? ? 'active' : ''
+  end
+
+  def set_hidden(method_type)
+    case method_type
+    when :unread_replies
+      has_unread_replies? ? '' : 'hidden'
+    when :replies
+      replies.present? ? '' : 'hidden'
+    end
+  end
+
+  def set_unread
+    has_unread_replies? ? 'unread' : ''
+  end
 end
