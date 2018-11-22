1d0d3d87710f90ebbaf9c100268a5eb433024b72
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-11-22 17:21:06 +0900

    feat: add repos.js

--- app/controllers/reviewers/github/contents_controller.rb
+++ app/controllers/reviewers/github/contents_controller.rb
@@ -4,8 +4,7 @@ class Reviewers::Github::ContentsController < Reviewers::BaseController
 
   # 第一階層のレポジトリファイルを取得
   def get_contents
-    Rails.logger.debug params[:file_type]
-    # @MEMO 単一も複数形の変数
+    breadcrumbs, breadcrumb_paths = new_breadcrumbs
     res = Github::Request.github_exec_fetch_repo_contents!(@repo, params[:path])
     res =
       case params[:file_type]
@@ -17,10 +16,10 @@ class Reviewers::Github::ContentsController < Reviewers::BaseController
           paths << content[:path]
           types << content[:type]
         end
-        dir_response(names, paths, types)
+        dir_response(names, paths, types, breadcrumbs, breadcrumb_paths)
       when 'file'
         content = ActiveSupport::HashWithIndifferentAccess.new(res)
-        file_response(content)
+        file_response(content, breadcrumbs, breadcrumb_paths)
       else
         names, paths, types = [], [], []
         res.each do |content|
@@ -29,10 +28,24 @@ class Reviewers::Github::ContentsController < Reviewers::BaseController
           paths << content[:path]
           types << content[:type]
         end
-        dir_response(names, paths, types)
+        dir_response(names, paths, types, breadcrumbs, breadcrumb_paths)
       end
     render json: res
   end
+
+  # def get_parent_dirs
+  #   parent_dir = params[:path].gsub(%r"#{params[:name]}$",'')
+  #   Rails.logger.debug parent_dir
+  #   res = Github::Request.github_exec_fetch_repo_contents!(@repo, parent_dir)
+  #   names, paths, types = [], [], []
+  #   res.each do |content|
+  #     content = ActiveSupport::HashWithIndifferentAccess.new(content)
+  #     names << content[:name]
+  #     paths << content[:path]
+  #     types << content[:type]
+  #   end
+  #   render json: dir_response(names, paths, types)
+  # end
 
   private
 
@@ -40,15 +53,47 @@ class Reviewers::Github::ContentsController < Reviewers::BaseController
     @repo = Repo.find(params[:repo_id])
   end
 
-  def dir_response(names, paths, types)
+  def new_breadcrumbs
+    return [], [] unless params[:path].present?
+    path = 
+      if params[:file_type].eql?('file')
+        params[:path].gsub(%r"#{params[:name]}$",'')
+      else
+        params[:path]
+      end
+    breadcrumbs = path.split('/')
+    breadcrumb_paths = []
+    Rails.logger.debug "breadcrumbs: #{breadcrumbs}"
+    if breadcrumbs.size > 1
+      breadcrumbs.each.with_index do |breadcrumb, index|
+        Rails.logger.debug breadcrumb
+        href = ''
+        index = index.to_i + 1
+        index.times do |time|
+          time = time if (index.to_i - 1).eql?(time.to_i)
+          href += '/' + breadcrumbs[time.to_i]
+        end
+        href = '/' + breadcrumbs[0] if index.eql?(0)
+        breadcrumb_paths << href
+      end
+    else
+      breadcrumb_paths << '/' + breadcrumbs[0]
+    end
+    return breadcrumbs, breadcrumb_paths
+  end
+
+  def dir_response(names, paths, types, breadcrumbs, breadcrumb_paths)
     {
       names: names.reverse,
       paths: paths.reverse,
-      types: types.reverse
+      types: types.reverse,
+      breadcrumbs: breadcrumbs,
+      breadcrumb_paths: breadcrumb_paths,
+      type: 'dir'
     }
   end
 
-  def file_response(res)
+  def file_response(res, breadcrumbs, breadcrumb_paths)
     highlight_content = []
     content = Base64.decode64(res[:content]).force_encoding('UTF-8')
 
@@ -58,7 +103,9 @@ class Reviewers::Github::ContentsController < Reviewers::BaseController
       name: res[:name],
       path: res[:path],
       type: res[:type],
-      content: highlight_content.reverse
+      content: highlight_content.reverse,
+      breadcrumbs: breadcrumbs,
+      breadcrumb_paths: breadcrumb_paths
     }
   end
 end
--- app/javascript/packs/repos.js
+++ app/javascript/packs/repos.js
@@ -9,7 +9,8 @@ $(document).on('click', '.file, .dir', function (e) {
   $('.table').empty()
   fileType = $(this).attr('class')
   repoId = $('.page-header').attr('repo-id')
-  path = $(this).attr('data-path')
+	path = $(this).attr('data-path')
+  name = $(this).attr('data-name')
   $.ajax({
     type: 'GET',
     url: `/reviewers/github/contents/get_contents`,
@@ -17,9 +18,38 @@ $(document).on('click', '.file, .dir', function (e) {
     data: {
       repo_id: repoId,
       path: path,
-      file_type: fileType
+			file_type: fileType,
+			name: name
     },
-  }).done(function (data) {
+	}).done(function (data) {
+		console.log(data.breadcrumbs)
+    console.log(data.breadcrumb_paths)
+    // 第二階層以下であれば
+    if (data.breadcrumbs.length > 0) {
+      breadcrumbElem = $('.breadcrumbs')
+      breadcrumbElem.empty()
+      repoName = $('.path').attr('repo-name')
+      topBreadcrumb = $(`<a href='/reviewers/repos/${$('.page-header').attr('repo-id')}'>${repoName}</a><span> / </span>`)
+      topBreadcrumb.appendTo(breadcrumbElem)
+      for (i = 0; i < data.breadcrumbs.length; i++) {
+        first_is_file = data.breadcrumbs.length == 1 && data.type == 'file'
+        last_is_dir = data.breadcrumbs[i] == data.breadcrumbs[data.breadcrumbs.length - 1] && data.type == 'dir'
+        except = first_is_file || last_is_dir
+        console.log(data.breadcrumbs.size)
+        console.log(data.type)
+        console.log(data.type == 'file')
+        if (except) {
+          continue;
+        }
+        breadcrumb = $(`
+          <span>
+            <a href='#' data-path='${data.breadcrumb_paths[i]}'>${data.breadcrumbs[i]}</a>
+            <span> / </span>
+          </span>
+        `)
+        breadcrumb.appendTo(breadcrumbElem)
+      }
+    }
     if (data.type == 'file') {
       highlightContent = data.content
       for (i = 0; i < highlightContent.length + 1; i++) {
@@ -44,27 +74,34 @@ $(document).on('click', '.file, .dir', function (e) {
             </tr>
           </tbody>
 				`)
-				$('.path').text(data.path)
 				$('.panel-title').text(data.name)
-        tbody.appendTo('table')
+				tbody.appendTo('table.main')
       }
+      breadcrumb = $(`
+        <span>${data.name}</span>
+      `)
+      breadcrumb.appendTo(breadcrumbElem)
     } else {
       names = data.names
       for (i = 0; i < names.length; i++) {
         tbody = $(`
 				<tbody>
 					<td>
-						<a href='#' data-path='${data.paths[i]}' class='${data.types[i]}'>
+						<a href='#' data-path='${data.paths[i]}' data-name='${data.names[i]}' class='${data.types[i]}'>
 							${data.names[i]}
 						</a>
 					</td>
 				</tbody>
 			`)
-        tbody.appendTo('table')
+				tbody.appendTo('table.main')
       }
+      breadcrumb = $(`
+        <span>${data.breadcrumbs[data.breadcrumbs.length - 1]}</span>
+      `)
+      breadcrumb.appendTo(breadcrumbElem)
     }
     $('img').addClass('hidden')
-    $('.panel').removeClass('hidden')
+    $('.panel.main').removeClass('hidden')
   }).fail(function (data) {
     issueList.text('issueの取得に失敗しました')
     $('#loader').addClass('hidden')
@@ -91,10 +128,10 @@ function setContents(repoId) {
 					</td>
 				</tbody>
 			`)
-      tbody.appendTo('table')
+			tbody.appendTo('table.main')
     }
     $('img').addClass('hidden')
-    $('.panel').removeClass('hidden')
+		$('.panel.main').removeClass('hidden')
   }).fail(function (data) {
     issueList.text('issueの取得に失敗しました')
     $('#loader').addClass('hidden')
