89489e06155f4e9cc0b962a9b805f96e0fc221b8
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-11-23 22:56:43 +0900

    fix: modified search.js

--- app/controllers/reviewers/contents_controller.rb
+++ app/controllers/reviewers/contents_controller.rb
@@ -5,7 +5,48 @@ class Reviewers::ContentsController < Reviewers::BaseController
   def index
     if params[:keyword].present?
       res = Github::Request.github_exec_search_contents_scope_repo!(@repo, params[:keyword])
-      Rails.logger.debug res.to_yaml
+      res = ActiveSupport::HashWithIndifferentAccess.new(res)
+      total_count = res[:total_count]
+      names, paths, = [], []
+      highlight_contents = []
+      text = []
+      indices = []
+      res[:items].each do |item|
+        item = ActiveSupport::HashWithIndifferentAccess.new(item)
+        names << item[:name]
+        paths << item[:path]
+        @highlight_content = []
+        item[:text_matches].each do |text_match|
+          @content = []
+          text_match = ActiveSupport::HashWithIndifferentAccess.new(text_match)
+
+          # Matches
+          matches = text_match[:matches]
+          matches = ActiveSupport::HashWithIndifferentAccess.new(matches.first)
+          text << matches[:text]
+
+          # Highlight Contents
+          fragment = text_match[:fragment]
+          fragment.each_line do |line|
+            line = line.gsub(' ', '&nbsp;')
+            line = line.gsub(/[<]/, "&lt;")
+            line = line.gsub(/[>]/, "&gt;")
+            line = line.gsub(text.first, "<span class='text'>#{text.first}</span>")
+            @content << line
+          end
+          @content.map! { |e| e ? e : '' }
+
+          @highlight_content << @content
+        end
+        highlight_contents << @highlight_content
+      end
+      render json: {
+        total_count: total_count,
+        names: names,
+        paths: paths,
+        highlight_contents: highlight_contents,
+        text: text
+      }
     end
   end
 
--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,4 +1,8 @@
 $(document).on('input', '.search-input', function (e) {
+	$('.loader').removeClass('hidden')
+	$('.results-wrapper').addClass('hidden')
+	$('.search-results-count').addClass('hidden')
+	$('.results-wrapper').empty()
 	keyword = $(this).val()
 	repoId = $('.page-header').attr('repo-id')
 	$.ajax({
@@ -9,9 +13,62 @@ $(document).on('input', '.search-input', function (e) {
 			keyword: keyword
 		},
 	}).done(function (data) {
-		// $('img').addClass('hidden')
-		// $('.panel-heading').removeClass('hidden')
-		// $('.code-wrapper').removeClass('hidden')
+		total_count = data.total_count
+		indices = data.indices
+		highlightContent = data.highlight_contents
+		names = data.names
+		// 検索結果一覧要素の作成
+		for (i = 0; i < total_count; i++) {
+			// コード要素の生成
+			codeWrapper = $(`
+				<div class='code-wrapper'>
+					<p class='filename'>${names[i]}</p>
+					<div class='panel panel-default'>
+					  <table class='table${i}'></table>
+					</div>
+				</div>
+			`)
+			codeWrapper.appendTo('.results-wrapper')
+			for (t = 0; t < highlightContent[i].length; t++) {
+				// DOMエレメント生成
+				for (k = 0; k < highlightContent[i][t].length; k++) {
+					border = ''
+					if (t > 0 && (highlightContent[i].length == t + 1) && (k == 0)) {
+						border = `
+							<tbody class='file'>
+								<tr>
+									<td class='border'>•••</td>
+									<td class='border'></td>
+								</tr>
+							</tbody>
+						`
+					}
+					var code = highlightContent[i][t][k]
+					// jQueryオブジェクトに変換
+					code = $(`<pre><code>${code}</code></pre>`)
+					// DOMエレメントに変換
+					var code = code[0]
+					// DOMエレメント出ないとハイライトしない
+					hljs.highlightBlock(code);
+					// 文字列で取得
+					code = code.outerHTML
+					tbody = $(`
+					  ${border}
+						<tbody class='file'>
+							<tr>
+								<td class='index'>#</td>
+								<td class='file-code'>${code}</td>
+							</tr>
+						</tbody>
+					`)
+					tbody.appendTo(`.table${i}`)
+				}
+			}
+		}
+		$('.search-results-count').text(`検索結果: ${total_count} 件`)
+		$('.loader').addClass('hidden')
+		$('.results-wrapper').removeClass('hidden')
+		$('.search-results-count').removeClass('hidden')
 	}).fail(function (data) {
 		// issueList.text('取得に失敗しました')
 		// $('#loader').addClass('hidden')
