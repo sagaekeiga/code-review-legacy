f11a04b39ead878d8eecb6e8065d581b380c4ae8
sagae minami <gw.feltmyheart@gmail.com>
2018-11-23 13:29:24 +0900

    fix:navbar_error

--- app/controllers/reviewers_controller.rb
+++ app/controllers/reviewers_controller.rb
@@ -3,9 +3,9 @@ class ReviewersController < Reviewers::BaseController
   skip_before_action :check_reviewer_status, only: %i(pending integrations)
 
   def dashboard
-    @repo = current_reviewer.repos.includes(:resource).order(updated_at: :desc).limit(10)
-    @pulls = Pull.includes(:repo).joins(:repo).request_reviewed.merge(current_reviewer.repos).order(updated_at: :desc).page(params[:page])
-    @pending_reviews = current_reviewer.reviews.where(event: :pending).includes(:pull)
+    @repos = current_reviewer.repos.includes(:resource).order(updated_at: :desc).limit(10)
+    @pulls = Pull.includes(:repo).joins(:repo).request_reviewed.merge(current_reviewer.repos).order(created_at: :desc).page(params[:page])
+    @pending_reviews = current_reviewer.reviews.pending.includes(:pull)
   end
 
   def my_page
