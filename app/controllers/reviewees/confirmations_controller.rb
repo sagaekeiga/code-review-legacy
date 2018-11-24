86a0ad0be823988c054fd881f8d433116b4b6e48
sagae minami <gw.feltmyheart@gmail.com>
2018-11-25 01:53:29 +0900

    fix:after_confirmed_redirect

--- app/controllers/reviewees/confirmations_controller.rb
+++ app/controllers/reviewees/confirmations_controller.rb
@@ -26,6 +26,6 @@ class Reviewees::ConfirmationsController < Devise::ConfirmationsController
 
   # The path used after confirmation.
   def after_confirmation_path_for(resource_name, resource)
-    stored_location_for(resource) || reviewees_dashboard_url
+    stored_location_for(resource) || reviewers_integrations_url
   end
 end
