788e461270da90ff520bb03cd5510b4b1bdfd514
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-11-27 10:36:11 +0900

    fix: modified search.js

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,12 +1,12 @@
 var stack = [];
 document.getElementById('searchInput').addEventListener('keyup', function () {
-	stack.push(1);
-	setTimeout($.proxy(function () {
-		stack.pop();
-		if (stack.length == 0) {
-			Search($(this))
-			stack = [];
-		}
+  stack.push(1);
+  setTimeout($.proxy(function () {
+    stack.pop();
+    if (stack.length == 0) {
+      Search($(this))
+      stack = [];
+    }
 	}, this), 300);
 });
 
