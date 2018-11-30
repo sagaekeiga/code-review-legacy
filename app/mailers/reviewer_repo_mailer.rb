626db380039ce01cc1388d9414307deb6ced10f8
sagae minami <gw.feltmyheart@gmail.com>
2018-12-01 00:00:33 +0900

    fix:repo_asign_notice

--- app/mailers/reviewer_repo_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ReviewerRepoMailer < ApplicationMailer
-  def repo_assign_notice(repo_assign_mail)
-    @repo = repo_assign_mail.repo
-    @reviewer = repo_assign_mail.reviewer
-    mail(subject: 'Mergeeからリポジトリをアサインがされました', to: @reviewer.email)
-  end
-end
