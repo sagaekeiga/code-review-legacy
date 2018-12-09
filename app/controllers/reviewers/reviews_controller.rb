c468967849ea8bf335de0495e8b2312a3e9133cd
Keiga Sagae <sagae5.28rujeae@gmail.com>
2018-12-09 19:26:04 +0900

    fix: add check_show

--- app/controllers/reviewers/reviews_controller.rb
+++ app/controllers/reviewers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class Reviewers::ReviewsController < Reviewers::BaseController
   before_action :set_review, only: %i(show edit update)
   before_action :set_pull, only: %i(view_check new create show edit update)
+  before_action :check_show, only: %i(show edit)
   before_action :check_assign, only: %i(new edit show)
   before_action :set_numbers, only: %i(new show edit)
   before_action :set_commits, only: %i(new show edit)
@@ -54,6 +55,10 @@ class Reviewers::ReviewsController < Reviewers::BaseController
     unless current_reviewer.assign_to!(@pull)
       return redirect_to view_check_reviewers_pull_reviews_path(@pull), warning: t('reviewers.reviews.messages.already')
     end
+  end
+
+  def check_show
+    return redirect_to reviewers_pull_review_replies_url(@pull, review_id: @review.id) if @review.approve? || @review.comment?
   end
 
   def set_changed_files
