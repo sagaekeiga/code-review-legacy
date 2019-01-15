aa991a9823e6d42d30820596d17fc7e8cc71c9ac
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-01-15 21:50:08 +0900

    fix: modified omuniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -16,7 +16,16 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   end
 
   configure do |config|
-  config.path_prefix = '/auth'
-  config.full_host = "https://" + ENV['WEB_DOMAIN'] if Rails.env.production?
+    config.path_prefix = '/auth'
+    config.full_host = lambda do |env|
+      protcol = Rails.env.production? ? 'https://' : 'http://'
+      case env['HTTP_HOST'].split('.').first
+      when 'reviewer'
+        protcol + ENV['REVIEWER_DOMAIN']
+      when 'app'
+        protcol + ENV['WEB_DOMAIN']
+      else
+      end
+    end
   end
 end
