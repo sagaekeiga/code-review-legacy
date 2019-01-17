cfa39524cb53efd691d62e40766cac480135a7dc
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-01-17 17:57:36 +0900

    feat: add reviewers#index

--- app/controllers/admins/reviewers_controller.rb
+++ app/controllers/admins/reviewers_controller.rb
@@ -1,5 +1,9 @@
 class Admins::ReviewersController < Admins::BaseController
   before_action :set_reviewer, only: %i(show update)
+  def index
+    @reviewers = Reviewer.all.includes(:github_account).order(created_at: :desc)
+  end
+
   def show
   end
 
