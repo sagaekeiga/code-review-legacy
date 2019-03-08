a0ce2c43213ab3ed89a14075d4379603e8bf2ebc
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-03-08 23:49:45 +0900

    fix: reviews.js

--- app/controllers/reviewers/review_comments_controller.rb
+++ app/controllers/reviewers/review_comments_controller.rb
@@ -2,18 +2,16 @@ require 'action_view'
 require 'action_view/helpers'
 include ActionView::Helpers::DateHelper
 class Reviewers::ReviewCommentsController < Reviewers::BaseController
-  before_action :set_changed_file, only: %i(create)
-  before_action :set_pull, only: %i(create)
   before_action :set_review_comment, only: %i(destroy update show)
 
   def create
     reviewer = Reviewer.find(params[:reviewer_id])
-    @changed_file = ChangedFile.find(params[:changed_file_id])
 
-    return render json: { status: 'failed' } if @changed_file.nil? || reviewer.nil?
+    return render json: { status: 'failed' } if reviewer.nil?
 
-    review_comment = @changed_file.review_comments.new(
+    review_comment = ReviewComment.new(
       event: :replied,
+      sha: params[:sha],
       position: params[:position],
       path: params[:path]&.gsub('\n', ''),
       body: params[:body],
@@ -57,14 +55,6 @@ class Reviewers::ReviewCommentsController < Reviewers::BaseController
   end
 
   private
-
-  def set_changed_file
-    @changed_file = ChangedFile.find(params[:changed_file_id])
-  end
-
-  def set_pull
-    @pull = @changed_file.pull
-  end
 
   def set_review_comment
     @review_comment = ReviewComment.find(params[:id])
