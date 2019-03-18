bda54a45e3d78b6bf6061451eae316bf7eedb8db
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-03-18 21:02:43 +0900

    fix: modifeid

--- app/services/analyze_files_service.rb
+++ app/services/analyze_files_service.rb
@@ -30,7 +30,7 @@ class AnalyzeFilesService
         end
         { body: I18n.t('analysis.template', rbp_outputs: format_outputs.join, errors_count: outputs.count).gsub('"', '').to_s }.to_json
       else
-        { body: I18n.t('analysis.fixed') }.to_json
+        { body: I18n.t('analysis.fixed').gsub('"', '').to_s }.to_json
       end
     issue_comment = pull.issue_comments.find_or_initialize_by(status: :analysis)
     if issue_comment.persisted?
