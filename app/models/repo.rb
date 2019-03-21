7aa84f6b40ce66c4e20a65b82bd40f5c98a65532
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-03-22 00:35:06 +0900

    feat: add has_rbp?

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -147,6 +147,16 @@ class Repo < ApplicationRecord
     end
   end
 
+  #
+  # Rails Best Practices を導入しているかどうかを返す
+  # @return [Boolean]
+  #
+  def has_rbp?
+    repo_analyses.exists?(
+      static_analysis: StaticAnalysis.rails_best_practices.first
+    )
+  end
+
   def reviewee?(current_reviewee)
     resource_type.eql?('Reviewee') && resource_id.eql?(current_reviewee.id)
   end
@@ -155,10 +165,20 @@ class Repo < ApplicationRecord
     resource_type.eql?('Org') && current_reviewee.orgs.exists?(id: resource_id)
   end
 
-  def introduced(static_analysis)
+  #
+  # 該当する 静的解析が導入されているかどうかを返す
+  # @param [StaticAnalysis] static_analysis 静的解析
+  # @return [Boolean]
+  #
+  def introduced?(static_analysis)
     repo_analyses.exists?(static_analysis_id: static_analysis.id)
   end
 
+  #
+  # 該当する静的解析を導入していれば、その解析ツールの id を返す
+  # @param [StaticAnalysis] static_analysis 静的解析
+  # @return [Integer/NilClass]
+  #
   def analysis_id(static_analysis)
     repo_analyses.find_by(static_analysis_id: static_analysis.id)&.id
   end
