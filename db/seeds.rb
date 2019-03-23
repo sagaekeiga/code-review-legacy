be4a09e6c4ffaf906090e937850c6140c2657cee
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-03-23 16:06:58 +0900

    feat: add update_check_runs

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ languages = YAML.load(languages_yml)
 languages.each do |language|
   Tag.create(name: language.flatten[0].to_s)
 end
+
+StaticAnalysis.create(
+  title: 'Rails Best Practices',
+  search_name: :rails_best_practices
+)
