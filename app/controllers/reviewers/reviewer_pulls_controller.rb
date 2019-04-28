b4d666459ac8baeca28078ca14ecb2c3db817ff9
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-04-28 12:37:23 +0900

    add flash reviewer_pulls

--- app/controllers/reviewers/reviewer_pulls_controller.rb
+++ app/controllers/reviewers/reviewer_pulls_controller.rb
@@ -1,13 +1,12 @@
 class Reviewers::ReviewerPullsController < Reviewers::BaseController
-  # skip_before_action :set_skill!, only: %i(update)
-  # skip_before_action :check_reviewer_status, only: %i(update)
-
   def create
     @pull = Pull.friendly.find(params[:pull_token])
-    if false
-      redirect_to file_reviewers_pull_reviews_path(@pull)
+    if current_reviewer.assign_to(@pull)
+      redirect_to file_reviewers_pull_reviews_path(@pull), success: t('.success')
     else
       @repo = @pull.repo
+      @changed_files = Pull::ChangedFileDecorator.decorate_collection @pull.changed_files
+      flash[:danger] = t '.failed'
       render 'reviewers/changed_files/index'
     end
   end
