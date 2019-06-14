46eeeda83d4d11cd8023780de4b1695fe2a278bd
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-06-15 00:15:28 +0900

    scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
+class WebDomainConstraint
+  # Review Appsでは毎回ドメインが変更されるのでドメイン制約をつけない
+  def self.matches?(request)
+    ENV['REVIEW_APP'].present? || request.host == (ENV['WEB_DOMAIN'])
+  end
+end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  # webサービス用のドメイン
+  #
+  constraints(WebDomainConstraint) do
+    # トップページ
+    root 'welcome#index'
+  end
 end
