4c48b9dc2c423deb9b316a96873102bc67d846b7
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-06-21 18:13:59 +0900

    can find by tag_name

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,6 +1,12 @@
 class FeedController < ApplicationController
   def index
-    @pulls = Pull.request_reviewed.order(updated_at: :desc).page(params[:page]).decorate
+    @pulls =
+      if params[:tag_name].present?
+        @tag = Tag.find_by(name: params[:tag_name])
+        @tag.pulls.order(updated_at: :desc).page(params[:page]).decorate
+      else
+        Pull.request_reviewed.order(updated_at: :desc).page(params[:page]).decorate
+      end
     respond_to do |format|
       format.rss { render layout: false }
     end
