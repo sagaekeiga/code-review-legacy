c8a397546ca8b005607e289451a25545745cd640
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-06-22 19:18:26 +0900

    tagify

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,5 +1,6 @@
 class ReposController < Users::BaseController
   def show
+    @repos = current_user.repos
     @repo = current_user.repos.find(params[:id])
     @pulls = @repo.pulls.opened.decorate
     @tags =  Tag.all
