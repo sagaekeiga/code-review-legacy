9fa9e16c2e41dfba17a3ae03408a82ec9cd0f61c
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-06-30 13:46:58 +0900

    fix remote url

--- app/decorators/pull_decorator.rb
+++ app/decorators/pull_decorator.rb
@@ -2,7 +2,7 @@ class PullDecorator < ApplicationDecorator
   delegate_all
   # GitHub上のプルリクエストへのリンクを返す
   def remote_url
-    "https://github.com/#{full_name}/pull/#{number}"
+    "https://github.com/#{full_name}/pull/#{model.number}"
   end
 
   def number
