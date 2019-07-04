06e93ad83cd8a489aa2e827db878b668964ac3ff
sagaekeiga <sagae5.28rujeae@gmail.com>
2019-07-04 22:21:23 +0900

    fix rubocop

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
-    tags = Tag.where("name ILIKE ?", "%#{params[:term]}%").where.not(id: current_user.tags.pluck(:id))
+    tags = Tag.where('name ILIKE ?', "%#{params[:term]}%").where.not(id: current_user.tags.pluck(:id))
     render json: tags.to_json
   end
 end
--- app/controllers/user_tags_controller.rb
+++ app/controllers/user_tags_controller.rb
@@ -2,7 +2,8 @@ class UserTagsController < Users::BaseController
   def create
     tag = Tag.find_by(name: params[:name])
     return render json: { success: false } if tag.nil?
-    user_tag =  tag.user_tags.new(user: current_user)
-    return render json: { success: user_tag.save, tag: tag }
+
+    user_tag = tag.user_tags.new(user: current_user)
+    render json: { success: user_tag.save, tag: tag }
   end
 end
